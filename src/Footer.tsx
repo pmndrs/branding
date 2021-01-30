5736c6b974d49137e679cbbff02136c0fe6ac08b
Paul Henschel <drcmda@gmail.com>
2021-01-30 14:48:56 +0100

    overlay

---
+++ src/Footer.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Logo } from './Logo'
+
+type Props = {
+  date: string
+  year: string
+  link1: React.ReactNode
+  link2: React.ReactNode
+}
+
+export function Overlay({ date, year, link1, link2 }: Props) {
+  return (
+    <div className="pmndrs-menu">
+      <div>
+        <Logo style={{ width: 42, height: 42 }} color="#b0b0b0" />
+      </div>
+      <div>
+        <span>
+          <b>poimandres</b>
+        </span>
+        <a href="https://pmnd.rs">pmndrs.rs</a>
+        <a href="https://twitter.com/pmndrs">@pmndrs</a>
+      </div>
+      <div>
+        <span>{date}</span>
+        <span>{year}</span>
+      </div>
+      <div>
+        <span>Open Source</span>
+        <span>Developer collective</span>
+      </div>
+      <div style={{ width: '100%' }} />
+      <div>
+        {link1}
+        {link2}
+      </div>
+    </div>
+  )
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
+import './styles.css'
+
 export * from './Badge'
 export * from './Logo'
+export * from './Footer'
