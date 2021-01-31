c7f71aa42bbba741af03037243befe71c4699732
AlaricBaraou <alaric.baraou@gmail.com>
2021-01-31 17:38:05 +0100

    hide decorative SVG from screen readers

--- src/Badge.tsx
+++ src/Badge.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export function Badge({ url = 'https://twitter.com/pmndrs', ...props }) {
   return (
-    <a href={url} {...props}>
-      <svg width="150" height="32" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <a href={url} aria-label="Poimandres on Twitter" {...props}>
+      <svg aria-hidden="true" width="150" height="32" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path
           d="M144.34 0H5.66A5.66 5.66 0 000 5.66v19.812a5.66 5.66 0 005.66 5.66h138.68a5.66 5.66 0 005.66-5.66V5.66A5.66 5.66 0 00144.34 0z"
           fill="#F2F2F2"
