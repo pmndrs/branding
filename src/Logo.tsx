c73efe00e7a47d29fab653a6f4ec2e463dc15270
Bjorn Stromberg <bjorn@bjornstar.com>
2021-06-03 20:22:45 +0900

    Use camelCase for clip and fill rules

--- src/Logo.tsx
+++ src/Logo.tsx
@@ -6,7 +6,7 @@ export function Logo({ color = 'black', ...props }) {
       <rect x="280" y="560" width="240" height="240" fill="inherit" />
       <rect x="280" y="280" width="240" height="240" fill="inherit" />
       <rect y="280" width="240" height="240" fill="inherit" />
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M560 0H280V240H560V520H800V0H560Z" fill="inherit" />
+      <path fillRule="evenodd" clipRule="evenodd" d="M560 0H280V240H560V520H800V0H560Z" fill="inherit" />
     </svg>
   )
 }
